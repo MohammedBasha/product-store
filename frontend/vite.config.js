fad55e62de610038e00ee5a1bf30fb74b3533722
Mohammed Basha <mohammedbasha9191@gmail.com>
2025-11-14 09:00:44 +0200

    Remove viteStaticCopy plugin from Vite config
    Removed viteStaticCopy plugin configuration.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
-        viteStaticCopy({
-          targets: [
-            { src: '_redirects', dest: '' }
-          ]
-        })
+        react()
     ],
     server: {
         proxy: {
